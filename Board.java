0c6c9aa928c3bd901275267f88a065c400ab445d
Zzcnick <zzhen@stuy.edu>
2016-01-12 11:16:51 -0500

    Add constructor, change instance variable board

--- Board.java
+++ Board.java
@@ -1,6 +1,9 @@
+import java.util.*;
+import java.io.*;
+
 public class Board {
     private ArrayList<Card> deck;
-    private ArrayList<ArrayList<Card>> board; //can you do this?
+    private ArrayList[] board;
 
     public void generateDeck() {
     }
@@ -9,9 +12,11 @@ public class Board {
     }
 
     public Board() {
+	board = new ArrayList[3];
     }
 
     public Card get(int row, int col) {
+	return new Card();
     }
 
     //draw() means picking three new cards from the deck
@@ -25,9 +30,15 @@ public class Board {
     }
 
     public boolean setExists() {
+	return false;
     }
 
     public String toString() {
+	return "oof";
     }
 
+    // TESTING
+    public static void main(String [] args) {
+	Board board = new Board();
+    }
 }
