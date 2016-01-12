782b301436b589ba4c9b611bf954592c02a5bcb4
Sarah Yoon <syoon2@stuy.edu>
2016-01-11 19:06:31 -0500

    added Board.java skeleton, updated devlog

---
+++ Board.java
@@ -0,0 +1,33 @@
+public class Board {
+    private ArrayList<Card> deck;
+    private ArrayList<ArrayList<Card>> board; //can you do this?
+
+    public void generateDeck() {
+    }
+
+    public void shuffleDeck() {
+    }
+
+    public Board() {
+    }
+
+    public Card get(int row, int col) {
+    }
+
+    //draw() means picking three new cards from the deck
+    public void draw() {
+    }
+
+    //shiftLeft() and distribute() are for arranging the board nicely after card selection
+    public void shiftLeft() {
+    }
+    public void distribute() {
+    }
+
+    public boolean setExists() {
+    }
+
+    public String toString() {
+    }
+
+}
