6aa46967254d0c37150c10f1f4c1c92d1d26c8b5
Sarah Yoon <syoon2@stuy.edu>
2016-01-13 19:45:54 -0500

    made Set.java skeleton

---
+++ Set.java
@@ -0,0 +1,38 @@
+//class Set, acts as driver
+
+public class Set {
+    // INSTANCE VARIABLES -- ?? Can they really be called this?
+
+    // for "time per set" version of game
+    private ArrayList<float> times = new ArrayList<float>();
+
+    // for "sprint" version of game
+    private float timeLimit;
+    private int numSets;
+
+    // METHODS
+    public boolean isSet(Card a, Card b, Card c) {
+
+    }
+
+    public float avgTime() {
+
+    }
+
+    // DIFFERENT VERSIONS OF GAME
+    public void playTimeVersion() {
+
+    }
+
+    public void playSprintVersion() {
+
+    }
+
+    // MAIN
+    public static void main(String[] args) {
+
+    }
+}
+
+    
+    
