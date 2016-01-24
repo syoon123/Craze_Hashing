f276ab4fe71ea35e238ec85dbe91094b45c4decd
Zzcnick <zzhen@stuy.edu>
2016-01-23 23:19:28 -0500

    Add user interaction for game selection

--- Set.java
+++ Set.java
@@ -17,31 +17,67 @@ public class Set {
 	" `Y8888P ,88P' 8 888888888888     8 8888\n";
     
     public static void main(String[] args) throws IOException, IndexOutOfBoundsException {
-	System.out.println(LOGO);
-	System.out.println("Choose an option:\n" +
-			   "\t1. Play\n" +
-			   "\t2. Instructions\n" +
-			   "\t3. About\n");
+	playloop:
+	while (true) { // Replay Loop
+	    System.out.println(LOGO);
+	    System.out.println("Choose an option:\n" +
+			       "\t1. Play\n" +
+			       "\t2. Instructions\n" +
+			       "\t3. About\n" +
+			       "\t4. Highscores\n" +
+			       "\t5. Quit\n");
 
-	// Game Selection
-	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	input.mark(1000);
-	while (true) { // Input Loop
-	    if (input.read() == 49) { // "1"
-		break;
-	    } else if (input.read() == 50) { // "2"
-		System.out.println("Instructions\n");
-		input.reset();
-	    } else if (input.read() == 51) { // "3"
-		System.out.println("About\n");
-		input.reset();
+	    // Game Selection
+	    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	    while (true) { // Input Loop
+		int _input = input.read();
+		if (_input == 49) { // "1"
+		    choosegame(input);
+		    break;
+		} else if (_input == 50) { // "2"
+		    System.out.println("Instructions\n");
+		} else if (_input == 51) { // "3"
+		    System.out.println("About\n");
+		} else if (_input == 52) { // "4" -- To Do Later
+		    System.out.println("Scores\n");
+		} else if (_input == 53) { // "5"
+		    break playloop;
+		}
+		clearBuffer(input);
+	    }
+
+	    System.out.println("Would you like to play again? Y/N");
+	    while (true) { // Play Again
+		int _input = input.read();
+		if (_input == 121 || _input == 89) // "Y" or "y"
+		    continue playloop;
+		if (_input == 110 || _input == 78) // "N" or "n"
+		    break playloop;
 	    }
 	}
-	
-	// Standard Game Mode
-	Set1.main(args);
+    }
 
-	// Sprint Mode
-	Set2.main(args);
+    public static void choosegame(BufferedReader input) throws IOException {
+	String[] args = new String[0];
+	System.out.println("Please select a game mode:\n" +
+			   "\t1. Standard\n" +
+			   "\t2. Sprint\n");
+
+	// Reader Input
+	while (true) {
+	    int _input = input.read();
+	    if (_input == 49) { // "1"
+	        Set1.main(args); // Standard Game Mode
+		break;
+	    } else if (_input == 50) { // "2"
+	        Set2.main(args); // Sprint Game Mode
+		break;
+	    }
+	}
+    }
+
+    public static void clearBuffer(BufferedReader input) throws IOException {
+	while (input.ready())
+	    input.read();
     }
 }
